53f20c9fac3234626228092df0a28e4cde0fe213
subramaniam.sahoo <subramaniam.sahoo@onebcg.com>
2014-12-08 17:53:46 +0530

    push for admin

--- controllers/sportsStore.js
+++ controllers/sportsStore.js
@@ -1,6 +1,8 @@
 angular.module("sportsStore")
         .constant("dataUrl", "http://localhost:2403/products")
-        .controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
+        .constant("orderUrl", "http://localhost:2403/orders")
+        .controller("sportsStoreCtrl", function($scope, $http, $location,
+                dataUrl, orderUrl, cart) {
 
             $scope.data = {};
             $http.get(dataUrl)
@@ -10,6 +12,21 @@ angular.module("sportsStore")
                     .error(function(error) {
                         $scope.data.error = error;
                     });
+
+            $scope.sendOrder = function(shippingDetails) {
+                var order = angular.copy(shippingDetails);
+                order.products = cart.getProducts();
+                $http.post(orderUrl, order)
+                        .success(function(data) {
+                            $scope.data.orderId = data.id;
+                            cart.getProducts().length = 0;
+                        })
+                        .error(function(error) {
+                            $scope.data.orderError = error;
+                        }).finally(function() {
+                    $location.path("/complete");
+                });
+            }
         });
 
          
