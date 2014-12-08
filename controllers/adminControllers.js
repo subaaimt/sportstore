d25f58c3b56e364d52cf24f162a7ba138aff422a
Subramaniam <subaaimt@gmail.com>
2014-12-09 00:50:16 +0530

    admin

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -1,7 +1,28 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+angular.module("sportsStoreAdmin")
+        .constant("authUrl", "http://localhost:2403/users")
+        .controller("authCtrl", function($scope, $http, $location, authUrl) {
+            $scope.authenticate = function(user, pass) {
+     
+                $http.post(authUrl, {
+                    username: user,
+                    password: pass
+                }, {
+                    withCredentials: true
+                }).success(function(data) {
+                    $location.path("/main");
+                }).error(function(error) {
+                    $scope.authenticationError = error;
+                });
+            };
+        })
+        .controller("mainCtrl", function($scope) {
+            $scope.screens = ["Products", "Orders"];
+            $scope.current = $scope.screens[0];
+            $scope.setScreen = function(index) {
+                $scope.current = $scope.screens[index];
+            };
+            $scope.getScreen = function() {
+                return $scope.current == "Products"
+                        ? "views/adminProducts.html" : "views/adminOrders.html";
+            };
+        });
